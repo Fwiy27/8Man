30d032a40fae58a8fc689302fc63f42e867ca131
Fwiy27 <112983265+Fwiy27@users.noreply.github.com>
2023-08-30 09:28:30 -0500

    updated

--- js/stats.js
+++ js/stats.js
@@ -1,57 +1,89 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Get select elements
-    document.getElementById("stat").style.visibility = "hidden";
+function init() {// Get select elements
     const weekSelect = document.getElementById('weekSelect');
     const skillSelect = document.getElementById('skillSelect');
+    console.log(skillSelect)
 
     weekSelect.addEventListener('change', updateTable);
     skillSelect.addEventListener('change', updateTable);
+}
 
-    function updateTable() {
-        document.getElementById("stat").style.visibility = "hidden";
-        // Remove existing event listeners from table headers
-        const headers = document.querySelectorAll('#csvTable th');
+function updateTable() {
+    document.getElementById("stat").style.visibility = "hidden";
+    // Remove existing event listeners from table headers
+    const headers = document.querySelectorAll('#csvTable th');
+    headers.forEach((header) => {
+        header.removeEventListener('click', sortTable);
+    });
 
-        const selectedWeek = weekSelect.value;
-        const selectedSkill = skillSelect.value;
+    const selectedWeek = weekSelect.value;
+    const selectedSkill = skillSelect.value;
 
-        // Check if both week and skill are selected
-        if (selectedWeek && selectedSkill) {
-            document.getElementById("stat").style.visibility = "visible";
-            var csvFileName;
-            if (selectedWeek === 'Week0') {
-                csvFileName = `../data/${selectedWeek}-${selectedSkill}-half.csv`;
-            } else {
-                csvFileName = `../data/${selectedWeek}-${selectedSkill}.csv`;
-            }
-
-            // Check if the file exists
-            fetch(csvFileName)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('File not found');
-                    }
-                    return response.text();
-                })
-                .then(data => {
-                    const tableContainer = document.querySelector('#csvTable');
-                    tableContainer.innerHTML = convertCSVToTable(data);
-                    searchTable();
-
-                })
-                .catch(error => {
-                    console.error('Error fetching data:', error);
-                    // Create a table with one header cell saying "No Data"
-                    document.querySelector('#csvTable').innerHTML = '<tr><th colspan="' + headers.length + '">No Data</th></tr>';
-                });
+    // Check if both week and skill are selected
+    if (selectedWeek && selectedSkill) {
+        document.getElementById("stat").style.visibility = "visible";
+        var csvFileName;
+        if (selectedWeek === 'Week0') {
+            csvFileName = `../data/${selectedWeek}-${selectedSkill}-half.csv`;
         } else {
-            document.querySelector('#csvTable').innerHTML = ''; // Clear the table if either week or skill is not selected
+            csvFileName = `../data/${selectedWeek}-${selectedSkill}.csv`;
         }
-    }
 
-    // Initial table setup (in case of pre-selected values)
-    updateTable();
-});
+        // Check if the file exists
+        fetch(csvFileName)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('File not found');
+                }
+                return response.text();
+            })
+            .then(data => {
+                const tableContainer = document.querySelector('#csvTable');
+                tableContainer.innerHTML = convertCSVToTable(data);
+                // Add event listeners to the table headers
+                const headers = document.querySelectorAll('#csvTable th');
+                headers.forEach((header, index) => {
+                    header.addEventListener('click', () => {
+                        const table = document.getElementById('csvTable');
+                        sortTable(table, index);
+                        console.log(index);
+                        console.log('test');
+
+                    });
+                });
+
+                searchTable();
+
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+                // Create a table with one header cell saying "No Data"
+                document.querySelector('#csvTable').innerHTML = '<tr><th colspan="' + headers.length + '">No Data</th></tr>';
+            });
+    } else {
+        document.querySelector('#csvTable').innerHTML = ''; // Clear the table if either week or skill is not selected
+    }
+}
+
+function sortTable(table, index) {
+    const tbody = table.querySelector('tbody');
+    const rows = Array.from(tbody.querySelectorAll('tr'));
+
+    const sortedRows = rows.sort((a, b) => {
+        const aCell = a.cells[index].textContent;
+        const bCell = b.cells[index].textContent;
+
+        if (!isNaN(aCell) && !isNaN(bCell)) {
+            // Compare numbers
+            return parseFloat(bCell) - parseFloat(aCell);
+        } else {
+            // Compare text
+            return aCell.localeCompare(bCell);
+        }
+    });
+
+    tbody.innerHTML = '';
+    sortedRows.forEach(row => tbody.appendChild(row));
+}
 
 function convertCSVToTable(csv) {
     const lines = csv.split('\n');
@@ -98,4 +130,9 @@ function searchTable() {
             }
         }
     }
+}
+
+window.onload = function() {
+    init();
+    updateTable();
 }
